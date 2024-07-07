ceaf33d632e3564e962ac5763b2d085b1d859510
Bansi <150083728+bsaboo@users.noreply.github.com>
2024-07-07 09:37:25 -0400

    Reactjs Project 1 initial commit

---
+++ src/components/TodoCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function TodoCard(props) {
+    const { children, handleDeleteTodo, index, handleEditTodo } = props
+    return (
+        <li className='todoItem'>
+            {children}
+            <div className='actionsContainer'></div>
+            <button onClick={() => handleEditTodo(index)}>
+                <i class="fa-solid fa-pen-to-square" ></i>
+            </button>
+            <button onClick={() => handleDeleteTodo(index)}><i class="fa-solid fa-trash"></i></button></li>
+    )
+}
